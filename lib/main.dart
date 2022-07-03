ad80c2ccc77c872b0998aecf3a242d13eae30d1e
Sorokin Vadym <s57111702@gmail.com>
2022-07-03 22:15:40 +0300

    Create main screen of application

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'main_screen.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+import 'package:chat_bot/bot_api/bot.dart';
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
+import 'package:chat_bot/base_module/module.dart';
+
+
+Bot botWithModules() {
+  var bot = Bot();
+  bot.modules.add(BaseModule());
+
+  return bot;
+}
+
+void main() {
+  var bot = botWithModules();
+
+  runApp(
+    ChangeNotifierProvider.value(value: bot,
+      child: const MyApp(),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
---
+++ lib/main_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_bot/bot_api/bot.dart';
+import 'package:provider/provider.dart';
+
+import 'base_module/messages.dart';
+
+class MainScreen extends StatelessWidget {
+  MainScreen({Key? key}) : super(key: key);
+
+  var userTextCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var bot = context.watch<Bot>();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello from app"),
+        ),
+        body: Column(children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: bot.messageHistory.length,
+                  itemBuilder: (context, i) {
+                    var msg = bot.messageHistory[i];
+                    return Container(
+                      child: msg.getWidget(),
+                      alignment: msg.isUser
+                          ? AlignmentDirectional.centerEnd
+                          : AlignmentDirectional.centerStart,
+                    );
+                  })),
+          Row(
+            children: [
+              Expanded(child: TextField(
+                controller: userTextCtrl,
+              )),
+              OutlinedButton(child: const Text("Send"),
+                  onPressed: () {
+                    var text = userTextCtrl.text;
+                    bot.addMessage(TextMessage(text, true));
+                    var result = bot.doCommand(text.split(' '));
+                    bot.addMessage(result);
+                    userTextCtrl.text = '';
+                  })
+            ],
+          )
+        ]));
+  }
+}
