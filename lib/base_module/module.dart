6f1467ad0f04aee8d9a3b6a6de54f70ad048e33d
Sorokin Vadym <s57111702@gmail.com>
2022-07-04 00:00:34 +0300

    Add clear command

--- lib/base_module/module.dart
+++ lib/base_module/module.dart
@@ -9,10 +9,19 @@ class BaseModule extends Module {
     return TextMessage(args.skip(1).join(' '), false, 'BaseModule');
   }
 
+  Message clear(Bot context, List<String> args){
+    var messageCount = context.messageHistory.length;
+    for (int i = context.messageHistory.length - 1; i >= 0; i--) {
+      context.deleteMessage(index: i);
+    }
+    return TextMessage("Було виделано $messageCount повідомлень!", false, 'BaseModule');
+  }
+
   @override
   Message? execute(Bot context, List<String> args) {
     switch (args[0]) {
       case '/reply': return replyMessage(context, args);
+      case '/clear': return clear(context, args);
     }
   }
 
