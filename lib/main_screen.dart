db575183ecbd045386909a145086bf2df5679e9a
Sorokin Vadym <s57111702@gmail.com>
2022-07-04 11:38:14 +0300

    Add scroll controller and message padding

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -10,10 +10,22 @@ class MainScreen extends StatelessWidget {
   MainScreen({Key? key}) : super(key: key);
 
   var userTextCtrl = TextEditingController();
+  var scrollMessageCtrl = ScrollController();
+
+  void scrollToEnd() {
+    final position = scrollMessageCtrl.position.maxScrollExtent;
+    scrollMessageCtrl.animateTo(position,
+          duration: const Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
+  }
 
   @override
   Widget build(BuildContext context) {
     var bot = context.watch<Bot>();
+    // Возможно перерассмотреть способ прокрутки
+    // Как вариант вынести область прокрути в другой класс
+    // или сделать более грамотный scrollMessageCtrl.addListener
+
+    WidgetsBinding.instance?.addPostFrameCallback((duration) => scrollToEnd());
 
     return Scaffold(
         appBar: AppBar(
@@ -21,7 +33,10 @@ class MainScreen extends StatelessWidget {
         ),
         body: Column(children: [
           Expanded(
+            child: Padding( padding: const EdgeInsets.symmetric(horizontal: 8),
               child: ListView.builder(
+                  controller: scrollMessageCtrl,
+                  shrinkWrap: true,
                   itemCount: bot.messageHistory.length,
                   itemBuilder: (context, i) {
                     var msg = bot.messageHistory[i];
@@ -31,9 +46,9 @@ class MainScreen extends StatelessWidget {
                           ? AlignmentDirectional.centerEnd
                           : AlignmentDirectional.centerStart,
                     );
-                  })),
+                  }))),
           Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
                 child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
